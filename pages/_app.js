17f329add1983156efac5d2067d34b98c7444bc6
马小波 <maxb@yunjiglobal.com>
2020-07-31 18:48:07 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import App from 'next/app'
+import 'antd/dist/antd.css'
 import '../styles/globals.css'
+import '../static/style/pages/common.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
