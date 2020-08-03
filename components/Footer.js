44c37b4294728db00dac3e9d10b72f980135d553
马小波 <maxb@yunjiglobal.com>
2020-08-03 11:26:58 +0800

    文章支持Markdown格式

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../static/style/components/footer.css'
+
+export default function Footer() {
+  return (
+    <div className="footer-div">
+      <div>系统由 React+Node+Ant Desgin驱动 </div>
+      <div>JSPang.com</div>
+    </div>
+  )
+}
